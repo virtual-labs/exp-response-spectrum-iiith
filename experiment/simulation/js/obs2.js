01b230614fb46d4dc707070ad81b2e1ae669754b
balammaboya <balamma38@gmail.com>
2022-07-13 18:35:21 +0530

    added new template design

--- experiment/simulation/js/obs2.js
+++ experiment/simulation/js/obs2.js
@@ -105,14 +105,14 @@ document.addEventListener('DOMContentLoaded', function(){
 			};
 
 			const layout = {
-				width: 450,
-				height: 450,
+				width: 250,
+				height: 250,
 				xaxis: {
 					title: {
 						text: 'Time Period',
 						font: {
 							family: 'Courier New, monospace',
-							size: 18,
+							size: 10,
 							color: '#000000'
 						}
 					},
@@ -122,7 +122,7 @@ document.addEventListener('DOMContentLoaded', function(){
 						text: Ytext,
 						font: {
 							family: 'Courier New, monospace',
-							size: 18,
+							size: 10,
 							color: '#000000'
 						}
 					}
@@ -143,7 +143,6 @@ document.addEventListener('DOMContentLoaded', function(){
 	const canvas = document.getElementById("main");
 	canvas.width = 1200;
 	canvas.height = 600;
-	canvas.style = "border:3px solid";
 	const ctx = canvas.getContext("2d");
 
 	const fill = "#A9A9A9", border = "black", lineWidth = 1.5;
@@ -240,3 +239,26 @@ document.addEventListener('DOMContentLoaded', function(){
 	main(); 
 	draw();
 })
+
+function openGraph(evt, graphName) {
+  var i, graphcontent;
+  graphcontent = document.getElementsByClassName("graphcontent");
+  for (i = 0; i < graphcontent.length; i++) {
+    graphcontent[i].style.display = "none";
+  }
+  document.getElementById(graphName).style.display = "block";
+  evt.currentTarget.className += "active";
+}
+function openGraph(evt, graphName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("graphcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(graphName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
